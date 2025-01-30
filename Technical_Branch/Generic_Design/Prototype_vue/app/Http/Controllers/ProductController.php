04d936bc421b82ddd5e68cf0244319e2484c5e28
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-01-30 23:33:45 +0000

    add the crud logic

--- Technical_Branch/Generic_Design/Prototype_vue/app/Http/Controllers/ProductController.php
+++ Technical_Branch/Generic_Design/Prototype_vue/app/Http/Controllers/ProductController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Repositories\ProductRepositoryInterface;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    protected $productRepository;
+
+    public function __construct(ProductRepositoryInterface $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $products = $this->productRepository->all();
+        return response()->json($products);
     }
 
     /**
@@ -28,7 +36,8 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $product = $this->productRepository->create($request->validated());
+        return response()->json($product);
     }
 
     /**
@@ -52,7 +61,8 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $product = $this->productRepository->update($product, $request->validated());
+        return response()->json($product);
     }
 
     /**
@@ -60,6 +70,14 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $this->productRepository->delete($product);
+        return response()->json(['message' => 'Product deleted successfully']);
+
+    }
+
+    public function restore($product)
+    {
+        $this->productRepository->restore($product);
+        return response()->json(['message' => 'Product restored successfully']);
     }
 }
