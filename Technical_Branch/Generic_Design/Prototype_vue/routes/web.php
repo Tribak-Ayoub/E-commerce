abbfbfa18240603d33d8f9355b863b836e08bbdb
Tribak-Ayoub <tribak.elyedri.ayoub@gmail.com>
2025-01-30 23:35:15 +0000

    add the route

--- Technical_Branch/Generic_Design/Prototype_vue/routes/web.php
+++ Technical_Branch/Generic_Design/Prototype_vue/routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
+Route::resource('products', ProductController::class);
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
